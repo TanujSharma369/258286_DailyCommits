7997a3d73742a244c57d5932779d10d0157ebb5a
tanuj <tanuj.com>
2021-04-22 17:35:13 +0530

    258286_shortcuts

---
+++ 6th.py
@@ -0,0 +1,12 @@
+shortcuts = {"lol":"laughing out loud",
+             "rofl":"rolling on the floor",
+             "lmk":"let me know",
+             "smh":"shaking my head"}
+
+if __name__ == "__main__":
+    test1 = "lol"
+    test2 = "rofl"
+    test3 = "smh"
+    print(shortcuts[test1])
+    print(shortcuts[test2])
+    print(shortcuts[test3])
